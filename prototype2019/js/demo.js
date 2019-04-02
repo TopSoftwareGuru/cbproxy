b2ff6f1ef692c489253cc0fc1f1e0a82321b1381
romanix <roman@example.com>
2019-04-02 21:00:17 +0200

    right pane activated

--- prototype2019/js/demo.js
+++ prototype2019/js/demo.js
@@ -4,7 +4,7 @@
 $(document).ready(function(){
 
   // hide the right pane
-  $("#right_pane").hide();
+  //$("#right_pane").hide();
 
   // TODO: later on, show it, if config.html says demo_mode = on
 
